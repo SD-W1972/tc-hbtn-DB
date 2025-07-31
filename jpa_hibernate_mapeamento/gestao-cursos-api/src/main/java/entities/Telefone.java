720b8484ea3dc8a6afae8bea532b7ad1742f6996
SD-W1972 <pernalongaesnobe@gmail.com>
2025-07-31 10:00:22 -0300

    feat: implementada entidade telefone

--- jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/entities/Telefone.java
+++ jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/entities/Telefone.java
@@ -1,4 +1,40 @@
 package entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Telefone {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String DDD;
+    private String numero;
+
+    public Telefone(String DDD, String numero) {
+        this.DDD = DDD;
+        this.numero = numero;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDDD() {
+        return DDD;
+    }
+
+    public void setDDD(String DDD) {
+        this.DDD = DDD;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
 }
