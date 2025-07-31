9182711101535b5de292684aeb9732223917f084
SD-W1972 <pernalongaesnobe@gmail.com>
2025-07-31 09:43:49 -0300

    feat: implementando entidades

--- jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/entities/Aluno.java
+++ jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/entities/Aluno.java
@@ -1,4 +1,63 @@
 package entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
 public class Aluno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nomeCompleto;
+    private String matricula;
+    private Date nascimento;
+    private String email;
+
+    public Aluno(String nomeCompleto, String matricula, Date nascimento, String email) {
+
+        this.nomeCompleto = nomeCompleto;
+        this.matricula = matricula;
+        this.nascimento = nascimento;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public Date getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(Date nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/entities/Professor.java
+++ jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/entities/Professor.java
@@ -1,4 +1,51 @@
 package entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Professor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nomeCompleto;
+    private String matricula;
+    private String email;
+
+    public Professor(String nomeCompleto, String matricula, String email) {
+        this.nomeCompleto = nomeCompleto;
+        this.matricula = matricula;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
