16fd106a5cb6c1e322c54ee579f1e544a39dbf25
SD-W1972 <pernalongaesnobe@gmail.com>
2025-08-01 19:56:04 -0300

    feat: adicionando objetos curso e material curso

--- jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/demo/GestaoCursosMain.java
+++ jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/demo/GestaoCursosMain.java
@@ -1,11 +1,16 @@
 package demo;
 
 import entities.Aluno;
+import entities.Curso;
+import entities.MaterialCurso;
 import entities.Professor;
 import models.AlunoModel;
 import models.CursoModel;
+
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 public class GestaoCursosMain {
     public static void main(String[] args){
@@ -19,13 +24,25 @@ public class GestaoCursosMain {
                 "00001", d1,
                 "fulaninho@mailcom");
 
-        Professor professor = new Professor(
+        Professor p1 = new Professor(
               "Professor maneiro",
               "00002",
               "professor@maneiro.com"
         );
 
+        List<Aluno> alunos = new ArrayList<>();
+        alunos.add(a1);
 
+        MaterialCurso materialCurso = new MaterialCurso(
+
+        );
+        Curso c1 = new Curso(
+          "Curso maneiro",
+          "CM",
+                alunos,
+
+
+        );
 
     }
 
