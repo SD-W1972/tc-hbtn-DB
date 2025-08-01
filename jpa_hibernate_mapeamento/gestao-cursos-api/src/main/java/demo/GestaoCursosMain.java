c7d37a50e06b2f3ab2e6b4da310a8331dc99b3fe
SD-W1972 <pernalongaesnobe@gmail.com>
2025-08-01 19:37:14 -0300

    feat: iniciando classe para populacao das tabelas do banco de dados

---
+++ jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/demo/GestaoCursosMain.java
@@ -0,0 +1,14 @@
+package demo;
+
+import models.AlunoModel;
+import models.CursoModel;
+
+public class GestaoCursosMain {
+    public static void main(String[] args){
+        AlunoModel alunoModel = new AlunoModel();
+        CursoModel cursoModel = new CursoModel();
+
+
+    }
+
+}
