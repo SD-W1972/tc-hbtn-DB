2923c3cf4897425f3b6b2042d5151de779af5cc2
SD-W1972 <pernalongaesnobe@gmail.com>
2025-08-01 19:52:30 -0300

    feat: adicionando objetos para popular tabelas de Aluno e Professor, respectivamente

--- jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/demo/GestaoCursosMain.java
+++ jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/demo/GestaoCursosMain.java
@@ -1,12 +1,30 @@
 package demo;
 
+import entities.Aluno;
+import entities.Professor;
 import models.AlunoModel;
 import models.CursoModel;
+import java.util.Calendar;
+import java.util.Date;
 
 public class GestaoCursosMain {
     public static void main(String[] args){
         AlunoModel alunoModel = new AlunoModel();
         CursoModel cursoModel = new CursoModel();
+        Date d1 = new Date();
+        Calendar calendar = Calendar.getInstance();
+        d1 = calendar.getTime();
+
+        Aluno a1 = new Aluno("Fulaninho de Tal",
+                "00001", d1,
+                "fulaninho@mailcom");
+
+        Professor professor = new Professor(
+              "Professor maneiro",
+              "00002",
+              "professor@maneiro.com"
+        );
+
 
 
     }
