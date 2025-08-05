b431f33eeabd3f427f7b7814b2341b6ef1e515d8
SD-W1972 <pernalongaesnobe@gmail.com>
2025-08-05 08:25:04 -0300

    feat: adicionados metodo create do CRUD

--- mongoDB/1/UsuarioOperations.java
+++ mongoDB/1/UsuarioOperations.java
@@ -1,6 +1,9 @@
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class UsuarioOperations {
     private MongoDBConnection mdbConnection;
@@ -10,6 +13,35 @@ public class UsuarioOperations {
     public UsuarioOperations(MongoDBConnection mdbConnection, MongoCollection<Document> collection) {
         this.mdbConnection = mdbConnection;
         this.collection = collection;
+    }
+
+    //CRUD - Create
+    public void insertUsuario(Usuario usuario){
+        try {
+            Document document = usuario.toDocument();
+            collection.insertOne(document);
+            System.out.println("Usuario inserido com sucesso: " + usuario.getNome());
+
+        }catch(Exception e) {
+            System.err.println("Erro ao inserir usuario: " + usuario.getNome());
+
+        }
+    }
+
+    //CRUD - Create many
+    public void insertUsuariosList(List<Usuario> usuarios){
+        List<Document> documentos = new ArrayList<>();
+        try {
+            for (Usuario u : usuarios) {
+                documentos.add(u.toDocument());
+            }
+            collection.insertMany(documentos);
+        }catch(Exception e){
+            System.err.println("Erro ao inserir lista de usuarios no banco de dados: ");
+            for(Usuario u : usuarios){
+                System.out.println(u.getNome());
+            }
+        }
     }
 
 
